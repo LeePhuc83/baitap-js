ce25292048b793afdf38f4de937cbc8bb16be3b7
LeePhuc83 <lephuccybersoft83@gmail.com>
2025-04-28 00:03:16 +0700

    upload source

--- hcn.js
+++ hcn.js
@@ -5,7 +5,28 @@ btnHcn.onclick = function () {
 
     let result = dai * rong;
 
-    let output = `Tổng tiền là: ${result}`;
+    let output = `diện tích là: ${result}`;
+
+    let pInfo_4 = document.getElementById("pInfo_4");
+    pInfo_4.innerHTML = output;
+
+    console.log(output);
+
+    pInfo_4.className = "info";
+
+
+
+
+
+}
+let btnCv = document.getElementById("btnCv");
+btnCv.onclick = function () {
+    let dai = document.getElementById("dai").value * 1;
+    let rong = document.getElementById("rong").value * 1;
+
+    let result = (dai + rong) / 2;
+
+    let output = `chu vi là: ${result}`;
 
     let pInfo_4 = document.getElementById("pInfo_4");
     pInfo_4.innerHTML = output;
