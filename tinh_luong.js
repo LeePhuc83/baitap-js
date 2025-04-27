ef346455688d648666453a7961728ae3e529e953
LeePhuc83 <lephuccybersoft83@gmail.com>
2025-04-27 22:50:10 +0700

    upload source

---
+++ tinh_luong.js
@@ -0,0 +1,39 @@
+/**
+ **Dau vao
+ -tao bien luongNgay va gan gia tri la 100000
+ -tao bien soNgayLam va cho phep user nhap
+ **Xu ly
+ -tao bien tongTien va gan gia tri la 0
+ -tongTien duoc gia tri moi la luongNgay * soNgayLam
+ **Dau ra
+ -In ra tongTien(log)
+ */
+
+
+
+let btnClick = document.getElementById("btnClick");
+btnClick.onclick = function () {
+    let luongNgay = document.getElementById("luongNgay").value * 1;
+    let soNgayLam = document.getElementById("soNgayLam").value * 1;
+
+    let result = luongNgay * soNgayLam;
+
+    let output = `Tổng tiền là: ${result}`;
+
+    let pInfo_1 = document.getElementById("pInfo_1");
+    pInfo_1.innerHTML = output;
+
+    console.log(output);
+
+    pInfo_1.style.backgroundColor = "yellow";
+    pInfo_1.style.color = "red";
+    pInfo_1.style.fontSize = "20px";
+    pInfo_1.style.textAlign = "center";
+
+
+    
+
+}
+
+
+
